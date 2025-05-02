2c64e2889c6e74fce6d30604ce8ba6953d44731e
Eman Saeed <148619269+emansa3ed@users.noreply.github.com>
2025-05-02 14:31:50 +0300

    Create adminRoutes.js

---
+++ routes/adminRoutes.js
@@ -0,0 +1,13 @@
+const express  = require("express");
+const router = express.Router();
+
+const {
+    getAllUsers,
+    deleteUser
+} = require("../controllers/adminController");
+const { protect, admin } = require('../middlewares/authMiddleware');
+
+router.get("/users", protect, admin ,getAllUsers);  
+router.delete("/users/:id" ,protect, admin, deleteUser);
+
+module.exports = router;
