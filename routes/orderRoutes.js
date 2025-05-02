c40a425a49dc58aaca72c644a1c0a1daa7478e8b
Eman Saeed <148619269+emansa3ed@users.noreply.github.com>
2025-05-02 14:38:40 +0300

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,18 +3,18 @@ const {
     createOrder, 
     getUserOrders, 
     getOrderById,
-    getAllOrders 
+    getAllOrders,
+    deletOrder 
 } = require('../controllers/orderController');
 const { protect, admin } = require('../middlewares/authMiddleware');
 
-// Apply protect middleware to all order routes
+
 router.post('/', protect, createOrder);
 router.get('/myorders', protect, getUserOrders);  
-
 router.get('/:id' , protect ,getOrderById);
+
 router.get('/', protect, admin, getAllOrders); // Admin only
 router.delete('/:id', protect, admin, deletOrder); // Admin only
-
 
 
 module.exports = router;
